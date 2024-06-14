9c0f4be38f7074ff89ec73c3d247b254df718407
blister-pack <andrecrujo@hotmail.com>
2024-06-14 19:11:57 +0200

    quite messy code rn lol

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
 
-for (let index = 0; index <= 16; index++) {
+for (let index = 0; index < 16; index++) {
     // 16*16 grid creation
-    
+    const lineDiv = document.createElement("div");
+    for (let index = 0; index < 16; index++) {
+        container.appendChild(lineDiv);
+    }
 }
