cda5b8614070466217a201bd2b5a51e6cad6d698
blister-pack <andrecrujo@hotmail.com>
2024-06-15 16:07:56 +0200

    created 16x16 grid creating function to change bg color of each cell

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const container = document.querySelector(".container");
+const pixel = document.querySelector(".container div div")
 
 for (let index = 0; index < 16; index++) {
     // 16*16 grid creation
-    const lineDiv = document.createElement("div");
+    // each of these divs is the start of a column
+    // they encapsulate each column
+    const columnDiv = document.createElement("div");
+    container.appendChild(columnDiv);
+    
     for (let index = 0; index < 16; index++) {
-        container.appendChild(lineDiv);
+        const lineDiv = document.createElement("div");
+        columnDiv.appendChild(lineDiv);
     }
+}
+
+pixel.addEventListener("hover", changeBackgroundColor);
+
+function changeBackgroundColor(event) {
+    let target = event.target
 }
